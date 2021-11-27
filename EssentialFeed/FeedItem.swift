7ddbc47ff9f22e92d92a73db43490b458be4aee5
John Gers <johnygers@gmail.com>
2021-11-26 19:02:42 -0500

    Create FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by John Gers on 11/26/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
+
+
